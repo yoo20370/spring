c6fcf744642c231ecc08faa0111f0a5e2d7a221d
yoo20370 <yoo20370@gmail.com>
2025-01-10 20:25:54 +0900

    spring code

--- spring_basic/core/src/main/java/hello/core/AppConfig.java
+++ spring_basic/core/src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,28 +8,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-// 애플리케이션 전체를 설정하고 구성함
+
+// 애플리케이션의 구성 정보를 담당하는 설정 정보에 붙이는 애너테이션
+@Configuration
 public class AppConfig {
-    
+
+    // @Bean은 스프링 컨테이너에 등록할 때 사용하는 애너테이션
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    // FixDiscountPolicy -> RateDiscountPolicy으로 변경하면 쉽게 할인 정책 변경 가능
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
-//    public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
-//    }
 }
--- spring_basic/core/src/main/java/hello/core/MemberApp.java
+++ spring_basic/core/src/main/java/hello/core/MemberApp.java
@@ -4,18 +4,24 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
-    // 순수 자바 코드, 자바 메서드 실행, 스프링 메서드 없음
     public static void main(String[] args) {
 
-        // 현재는 AppConfig를 통해서
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
 
-        // 기존에는 main 메서드에서 직접 생성해서 사용
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        // ApplicationContext가 스프링 컨테이너에 있는 모든 객체를 관리
+        // Annotation 기반으로 설정 파일을 사용하므로 AppConfig.class의 환경 설정 정보를 가지고 스프링이 객체들을 스프링 컨테이너에 집어 넣어서 관리
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Bean을 가져와야 함, 이 때 AppConfig에 @Bean을 사용한 메서드 이름이 컨테이너에 등록됨, 해당 메서드를 가져오도록 인자 전달, 두 번째 인자는 반환 타입
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
