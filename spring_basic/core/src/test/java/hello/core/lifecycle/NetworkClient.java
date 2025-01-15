6ecc01f9a1014dfd2fda05f0806d433699747e81
yoo20370 <yoo20370@gmail.com>
2025-01-15 14:13:25 +0900

    bean lifeCycle method

--- spring_basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring_basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
 
     private String url;
 
@@ -31,18 +31,18 @@ public class NetworkClient implements InitializingBean, DisposableBean {
     }
 
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    public void init() throws Exception {
         // 의존 관계 주입이 끝나면 호출
-        System.out.println("NetworkClient.afterPropertiesSet");
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
+
+    public void close() throws Exception {
         // 스프링 빈이 종료되기 직전
-        System.out.println("NetworkClient.destroy");
+        System.out.println("NetworkClient.close");
         disConnect();
     }
 }
