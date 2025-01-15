ec0ae56ec7171e3ca1f0ec5fb2f91b309554befe
yoo20370 <yoo20370@gmail.com>
2025-01-15 21:48:14 +0900

    scope, proxy

--- spring_basic/core/src/main/java/hello/core/common/MyLogger.java
+++ spring_basic/core/src/main/java/hello/core/common/MyLogger.java
@@ -5,13 +5,13 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
-@Scope(value="request")
-// 해당 빈은 HTTP 요청 당 하나씩 생성되고, HTTP 요청이 끝나는 시점에 소멸된다.
+@Scope(value="request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     private String uuid;
--- spring_basic/core/src/main/java/hello/core/web/LogDemoController.java
+++ spring_basic/core/src/main/java/hello/core/web/LogDemoController.java
@@ -12,23 +12,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
-    // 예외 발생한 이유
-    // 컨테이너에서 의존성 주입을 위해 myLogger를 가져오려고 하는데 스프링 빈에 없음
-    // request 스코프는 HTTP 요청이 오면 생성되고 끝나면 사라지기 때문
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
-    @ResponseBody // 문자열을 응답으로 반환하고 싶은 경우 ResponseBody 사용
+    @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        // HttpServletRequest - 자바에서 제공하는 표준 서블릿 규약,
-        // 이를 통해 고객 request 정보를 받을 수 있다.
-        // Provider를 사용했기 때문에 이 시점에 MyLogger 빈이 만들어진다. 그리고 컨테이너에서 가져온다.
-        MyLogger myLogger = myLoggerProvider.getObject();
-
         String requestURL  = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
+
         myLogger.log("controller test");
         logDemoService.logic("testId");
         return "OK";
