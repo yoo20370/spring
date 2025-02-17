210a9d475bf1a2663298577d539d01da3287fff2
yoo20370 <yoo20370@gmail.com>
2025-02-17 19:04:49 +0900

    20250217 190500 jpa basic

--- ex0-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex0-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,15 +2,41 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
 
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        // 데이터베이스 커넥션 하나 받았다고 생각하면 편하다.
         EntityManager em = emf.createEntityManager();
         //code
 
-        em.close();
+        EntityTransaction ts = em.getTransaction();
+
+        ts.begin();
+
+        try {
+
+            List<Member> selectMFromMember = em.createQuery("SELECT m FROM Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(8)
+                    .getResultList();
+
+            for (Member member : selectMFromMember) {
+                System.out.println("member = " + member);
+            }
+            
+            ts.commit();
+        } catch (Exception e) {
+            ts.rollback();
+        } finally {
+            // 애플리케이션 종료시 종료시켜줘야 함
+            em.close();
+        }
         emf.close();
     }
 }
